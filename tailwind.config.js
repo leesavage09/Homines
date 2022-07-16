fd0abf5affaaa07ca741074d62637ba009c3a92a
leesavage09 <leesavage09@gmail.com>
2022-07-16 14:50:16 +0300

    configure tailwind build

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'white': '#fff',
+      'grey': '#707070',
+      'olive': '#BDC6AB',
+    },
+    fontFamily: {
+      'base': 'Roboto, sans-serif'
+    },
+    fontSize: {
+      'base': '1rem', //16
+      'lg': '1.25rem',//20
+      'xl': '1.5rem', //25
+      '2xl': '2rem',  //30
+      '3xl': '2.18rem', //35
+      '4xl': '2.5rem',//40
+      '5xl': '2.81rem',//45
+    }
+  },
+  plugins: [],
+}
