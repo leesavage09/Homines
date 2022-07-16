6da1d4a246f6f992b00d992c1f178d5123c85b9e
Lee <leesavage09@gmail.com>
2022-07-16 10:47:07 +0300

    Added contentful

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,32 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = (props) => {
+  console.log('props', props.data);
+
+  return (
+    <Layout>
+      <Seo title="Page two" />
+      <h1>Hi from the second page</h1>
+      <p>
+        <img width={props.data.contentfulAsset.width} height={props.data.contentfulAsset.height} src={props.data.contentfulAsset.url} />
+      </p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+};
 
 export default SecondPage
+
+export const pageQuery = graphql`
+  query getMainPhoto {
+  contentfulAsset(title: {eq: "mainphoto"}) {
+    url
+    height
+    width
+  }
+}
+`
